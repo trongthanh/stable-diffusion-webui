6f34e7e0208ee99b4a15f20b0e074b993dd5ef32
Thanh Tran <trongthanh@gmail.com>
2024-07-22 23:31:35 +0700

    Add back extensions to empty_provisioning.sh

--- ai-dock/empty_provisioning.sh
+++ ai-dock/empty_provisioning.sh
@@ -13,6 +13,15 @@ PIP_PACKAGES=(
 )
 
 EXTENSIONS=(
+    "https://github.com/Mikubill/sd-webui-controlnet"
+    "https://github.com/deforum-art/sd-webui-deforum"
+    "https://github.com/adieyal/sd-dynamic-prompts"
+    "https://github.com/ototadana/sd-face-editor"
+    "https://github.com/AlUlkesh/stable-diffusion-webui-images-browser"
+    "https://github.com/hako-mikan/sd-webui-regional-prompter"
+    "https://github.com/Coyote-A/ultimate-upscale-for-automatic1111"
+    "https://github.com/Gourieff/sd-webui-reactor"
+    "https://github.com/Bing-su/adetailer.git"
 )
 
 CHECKPOINT_MODELS=(
