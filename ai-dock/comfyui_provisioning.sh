8fc9f9e1aa36d6d23b12fa56932255063df25915
Thanh Tran <trongthanh@gmail.com>
2024-07-29 14:06:48 +0700

    feat: add more useful nodes

--- ai-dock/comfyui_provisioning.sh
+++ ai-dock/comfyui_provisioning.sh
@@ -12,9 +12,11 @@ PYTHON_PACKAGES=(
 
 NODES=(
     "https://github.com/ltdrdata/ComfyUI-Manager"
+    "https://github.com/crystian/ComfyUI-Crystools"
     "https://github.com/cubiq/ComfyUI_essentials"
     "https://github.com/cubiq/ComfyUI_IPAdapter_plus"
     "https://github.com/cubiq/ComfyUI_InstantID"
+    "https://github.com/Fannovel16/comfyui_controlnet_aux"
 )
 
 CHECKPOINT_MODELS=(
